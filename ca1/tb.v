6fb7e33ff7649a70bd23ed52d7a120877670aed7
mahdi vajhi <mvajhimv@gmail.com>
2024-03-10 20:50:05 +0330

    Fix tb

--- ca1/tb.v
+++ ca1/tb.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 module tb();
-    reg [9:0]a_in=10'd56;
-    reg [9:0]b_in=10'd5;
+    reg [9:0]a_in=10'b1000001000;
+    reg [9:0]b_in=10'b0001110100;
     reg start=1'b0;
     reg clk=1'b0;
     reg sclr=1'b0;
@@ -13,7 +13,7 @@ module tb();
     always #10 clk = ~clk;
     initial begin
         #10 start=1'b1;
-
+        #1000 $stop;
     end
     
 
