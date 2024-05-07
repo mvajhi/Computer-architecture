56fe1924afa5d23b7fbf92796ba4be19069859d6
mohammadamintavanai <tavanaim@gmail.com>
2024-05-07 19:21:52 +0330

    djjdjd

---
+++ ca2/data_path.v
@@ -0,0 +1,15 @@
+module data_path(
+    input pcsel,
+    input regsel,
+    input extend_func,
+    input wereg,
+    input wedata,
+    input aluselb,
+    input aluop,
+    input outsel,
+    output op,
+    output func3,
+    output func7
+);
+
+endmodule
