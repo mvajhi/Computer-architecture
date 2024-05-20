d979a24ed53b16c8f3a870fba1ee3f9172501535
mahdi vajhi <mvajhimv@gmail.com>
2024-05-20 19:10:02 +0330

    Add tb

---
+++ ca3/tb.v
@@ -0,0 +1,17 @@
+module tb();
+    reg clk = 1'b0;
+    reg rst;
+
+    processor dut (
+        .clk(clk),
+        .rst(rst)
+    );
+
+    always #10 clk = ~clk;
+
+    initial begin
+        rst = 1;
+        #10 rst = 0;
+        #10000 $finish;
+    end
+endmodule
